fc4b44d750c0f75027f69597e38cb00109a47380
varchasvshri <40594226+varchasvshri@users.noreply.github.com>
2019-12-11 00:47:32 +0530

    Update ISM_paper_logic_code.py

--- ISM_paper_logic_code.py
+++ ISM_paper_logic_code.py
@@ -1,14 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 from collections import defaultdict 
-  
+import numpy as np
+
 #Class to represent a graph 
-class Graph: 
-  
+class Graph:  
     def __init__(self, vertices): 
         self.V = vertices 
   
@@ -23,40 +17,88 @@ class Graph:
       
     # Prints transitive closure of graph[][] using Floyd Warshall algorithm 
     def transitiveClosure(self,graph): 
-        '''reach[][] will be the output matrix that will finally 
-        have reachability values. 
-        Initialize the solution matrix same as input graph matrix'''
         reach =[i[:] for i in graph] 
-        '''Add all vertices one by one to the set of intermediate 
-        vertices. 
-         ---> Before start of a iteration, we have reachability value 
-         for all pairs of vertices such that the reachability values 
-          consider only the vertices in set  
-        {0, 1, 2, .. k-1} as intermediate vertices. 
-          ----> After the end of an iteration, vertex no. k is 
-         added to the set of intermediate vertices and the  
-        set becomes {0, 1, 2, .. k}'''
         for k in range(self.V): 
-              
-            # Pick all vertices as source one by one 
             for i in range(self.V): 
-                  
-                # Pick all vertices as destination for the 
-                # above picked source 
                 for j in range(self.V): 
-                      
-                    # If vertex k is on a path from i to j,  
-                       # then make sure that the value of reach[i][j] is 1 
                     reach[i][j] = reach[i][j] or (reach[i][k] and reach[k][j]) 
-  
-        self.printSolution(reach) 
-          
-g= Graph(4) 
-  
-graph = [[1, 1, 0, 1], 
-         [0, 1, 1, 0], 
-         [0, 0, 1, 1], 
-         [0, 0, 0, 1]] 
-  
-#Print the solution 
-g.transitiveClosure(graph)
+        return reach 
+
+def Level_Partioning(final):
+    common_mat = []
+    for i in range(n):
+        temp_reach = []
+        temp_antec = []
+        for j in range(n):
+            if(final[i][j]==1):
+                temp_reach.append(j)
+            if(final[j][i]==1):
+                temp_antec.append(j)
+        common_mat.append(temp_reach)
+        common_mat.append(temp_antec)
+    return common_mat
+
+def stop_crit(level):
+    for x in level:
+        if(x==0):
+            return True
+    return False        
+
+def xandy(final):
+    Driving_power = []
+    Dependence_power = []
+
+    for i in range(n):
+        countx=0
+        county=0
+        for j in range(n):
+            if(final[i][j]==1):
+                countx = countx + 1
+            if(final[j][i]==1):
+                county = county + 1
+        Driving_power.append(countx)
+        Dependence_power.append(county)
+    return Driving_power, Dependence_power
+
+def find_level(intersection_set, common_mat):
+    levels = np.zeros(n, dtype=int)
+    count = 1
+
+    while(stop_crit(levels)):
+        store = []
+        for i in range(n):
+            if(len(intersection_set[i])!=0 and set(intersection_set[i]) == set(common_mat[2*i])):
+                levels[i] = count
+                store.append(i)
+        count = count + 1
+        for x in store:
+            for i in common_mat:
+                if x in i: i.remove(x)
+            for i in intersection_set:
+                if x in i: i.remove(x)
+    return levels
+
+n = int(input('Dimension of your Initial Reachability matrix : '))
+area = input('name of input file : ')
+
+graph = np.loadtxt(area, usecols=range(n))
+
+g= Graph(n) 
+final = g.transitiveClosure(graph)
+Driving_power, Dependence_power = xandy(final)
+
+# g.printSolution(final)
+
+common_mat = Level_Partioning(final)
+
+intersection_set = []
+for i in range(n):
+    intersection_set.append(list(set(common_mat[2*i]) & set(common_mat[2*i + 1])))
+
+levels = find_level(intersection_set, common_mat)
+
+for i in range(n):
+    print('Level in TISM for E%d is %d'%(i+1,levels[i]))
+# print(levels)
+# print(intersection_set)
+# print(common_mat)
